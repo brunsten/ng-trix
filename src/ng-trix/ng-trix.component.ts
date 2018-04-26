2849ad24fa183726473e586f095b00dc2a5bc029
Marcus Brunsten <marcus.brunsten@gmail.com>
2018-04-26 21:04:23 +0200

    Added use cases

--- src/ng-trix/ng-trix.component.ts
+++ src/ng-trix/ng-trix.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-@Component(
-    {
-        selector: 'ng-trix',
-        templateUrl: './ng-trix.component.html'
-    }
-)
+@Component({
+  selector: 'ng-trix',
+  templateUrl: './ng-trix.component.html'
+})
 export class NgTrixComponent {
-    constructor() { }
+  ngModel = '([<b>ngModel</b>])';
+  formGroup = new FormGroup({
+    formControl: new FormControl('form<i>Control</i>', [Validators.required])
+  });
+  constructor() {}
 }
--- src/ng-trix/ng-trix.module.ts
+++ src/ng-trix/ng-trix.module.ts
@@ -1,9 +1,13 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { NgTrixComponent } from "./ng-trix.component";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgTrixComponent } from './ng-trix.component';
+import { MatInputModule } from '@angular/material';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [NgTrixComponent],
-    exports: [NgTrixComponent],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  imports: [CommonModule, MatInputModule, FormsModule, ReactiveFormsModule],
+  declarations: [NgTrixComponent],
+  exports: [NgTrixComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class NgTrixModule {}
