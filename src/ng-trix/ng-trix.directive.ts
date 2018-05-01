43a3036178f06c6f3e603312c8e2941da0658451
Marcus Brunsten <marcus.brunsten@playngo.com>
2018-05-01 21:34:15 +0200

    write value

--- src/ng-trix/ng-trix.directive.ts
+++ src/ng-trix/ng-trix.directive.ts
@@ -1,19 +1,39 @@
-import { Directive, Input, SimpleChanges, OnChanges } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Directive, Input, SimpleChanges, OnChanges, Inject, OnInit, Optional, Renderer2 } from '@angular/core';
+import { FormControl, FormGroup, Validators, NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[ngTrix]'
 })
-export class NgTrixDirective implements OnChanges {
-  @Input('ngTrix') trixElement: HTMLElement;
-  constructor() {}
+export class NgTrixDirective implements OnInit, OnChanges {
+  @Input('ngTrix') trixElement: any;
+  @Input() debug = false;
+  @Input() value: string;
+  constructor(@Optional() private _ngControl: NgControl, private _renderer: Renderer2) {
+    if (this.debug) {
+      // console.log(this._ngControl);
+    }
+  }
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes);
     if (changes['trixElement']) {
       this.initTrix();
     }
+    console.log(changes);
   }
+  ngOnInit(): void {}
   initTrix(): void {
+    console.log(this.trixElement);
+    if (this.trixElement) {
+      this._renderer.listen(this.trixElement, 'trix-change', (event: Event) => {
+        if (event.eventPhase === Event.AT_TARGET) {
+          if (this._ngControl) {
+            this._ngControl.valueAccessor.writeValue(this.trixElement.value);
+          }
+        }
+      });
+    }
+  }
 
+  setTrixValue(value: string): void {
+    this.trixElement.value;
   }
 }
